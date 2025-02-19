0b389cc0d6fae53ecf74b0e42b9902415f0d8193
root <root@7TGF1T3>
2025-02-19 17:40:21 +0800

    modified ci/cd

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 1.10.0"
   backend "s3" {
-    bucket = "tsanghan-tfstate"
+    bucket = "ausente-tstate"
     key    = "f5xc-cicd.tfstate"
     region = "ap-southeast-1"
   }
